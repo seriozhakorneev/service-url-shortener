ff3b42a38d0ad3598f9087de1f1cb60939191239
korneevsergey <seriozhakorneev@gmail.com>
2022-11-12 21:35:17 +0300

    tests
    Minor fixes in mocks
    Add few usecase.ShortenerUseCase tests.

--- internal/usecase/mocks/mocks.go
+++ internal/usecase/mocks/mocks.go
@@ -1,43 +1,43 @@
-package usecase_test
+package mocks
 
 import "context"
 
-type mockDigitiser struct {
+type MockDigitiser struct {
 	DigitFunc  func(s string) (int, error)
 	StringFunc func(i int) (string, error)
 	MaxFunc    func() int
 	LengthFunc func() int
 }
 
-func (m mockDigitiser) Digit(s string) (int, error) {
+func (m MockDigitiser) Digit(s string) (int, error) {
 	if m.DigitFunc != nil {
 		return m.DigitFunc(s)
 	}
 	return 0, nil
 }
 
-func (m mockDigitiser) String(i int) (string, error) {
+func (m MockDigitiser) String(i int) (string, error) {
 	if m.StringFunc != nil {
 		return m.StringFunc(i)
 	}
 	return "", nil
 }
 
-func (m mockDigitiser) Max() int {
+func (m MockDigitiser) Max() int {
 	if m.MaxFunc != nil {
 		return m.Max()
 	}
 	return 0
 }
 
-func (m mockDigitiser) Length() int {
+func (m MockDigitiser) Length() int {
 	if m.LengthFunc != nil {
 		return m.LengthFunc()
 	}
 	return 0
 }
 
-type mockUrlsRepo struct {
+type MockUrlsRepo struct {
 	CreateFunc  func(ctx context.Context, s string) (int, error)
 	RewriteFunc func(ctx context.Context, s string) (int, error)
 	GetURLFunc  func(ctx context.Context, i int) (string, error)
@@ -46,42 +46,42 @@ type mockUrlsRepo struct {
 	CountFunc   func(ctx context.Context) (int, error)
 }
 
-func (m mockUrlsRepo) Create(ctx context.Context, s string) (int, error) {
+func (m MockUrlsRepo) Create(ctx context.Context, s string) (int, error) {
 	if m.CreateFunc != nil {
 		return m.CreateFunc(ctx, s)
 	}
 	return 0, nil
 }
 
-func (m mockUrlsRepo) Rewrite(ctx context.Context, s string) (int, error) {
+func (m MockUrlsRepo) Rewrite(ctx context.Context, s string) (int, error) {
 	if m.RewriteFunc != nil {
 		return m.RewriteFunc(ctx, s)
 	}
 	return 0, nil
 }
 
-func (m mockUrlsRepo) GetURL(ctx context.Context, i int) (string, error) {
+func (m MockUrlsRepo) GetURL(ctx context.Context, i int) (string, error) {
 	if m.GetURLFunc != nil {
 		return m.GetURLFunc(ctx, i)
 	}
 	return "", nil
 }
 
-func (m mockUrlsRepo) GetID(ctx context.Context, s string) (int, error) {
+func (m MockUrlsRepo) GetID(ctx context.Context, s string) (int, error) {
 	if m.GetIDFunc != nil {
 		return m.GetIDFunc(ctx, s)
 	}
 	return 0, nil
 }
 
-func (m mockUrlsRepo) Touch(ctx context.Context, i int) error {
+func (m MockUrlsRepo) Touch(ctx context.Context, i int) error {
 	if m.TouchFunc != nil {
 		return m.TouchFunc(ctx, i)
 	}
 	return nil
 }
 
-func (m mockUrlsRepo) Count(ctx context.Context) (int, error) {
+func (m MockUrlsRepo) Count(ctx context.Context) (int, error) {
 	if m.CountFunc != nil {
 		return m.Count(ctx)
 	}
--- internal/usecase/shortener_test.go
+++ internal/usecase/shortener_test.go
@@ -1,1 +1,67 @@
 package usecase
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"service-url-shortener/internal/usecase/mocks"
+)
+
+func TestShortenerUseCase_Shorten_Errors(t *testing.T) {
+
+	tests := []struct {
+		expectedErr error
+		usecase     Shortener
+	}{
+		// uc.exist errors
+		{
+			expectedErr: fmt.Errorf(
+				"ShortenerUseCase - Shorten - %w",
+				fmt.Errorf("exist - uc.repo.GetID: %w",
+					fmt.Errorf("UrlsRepo - GetID - r.Pool.QueryRow.Scan: %w",
+						errors.New("sql error")))),
+
+			usecase: &ShortenerUseCase{
+				repo: mocks.MockUrlsRepo{
+					GetIDFunc: func(ctx context.Context, s string) (int, error) {
+						return 0,
+							fmt.Errorf("UrlsRepo - GetID - r.Pool.QueryRow.Scan: %w",
+								errors.New("sql error"))
+					},
+				},
+			},
+		},
+		{
+			expectedErr: fmt.Errorf(
+				"ShortenerUseCase - Shorten - %w",
+				fmt.Errorf("exist - uc.digitiser.String: %w",
+					errors.New("digitiser error"))),
+
+			usecase: &ShortenerUseCase{
+				repo: mocks.MockUrlsRepo{
+					GetIDFunc: func(ctx context.Context, s string) (int, error) {
+						return 0, nil
+					},
+				},
+				digitiser: mocks.MockDigitiser{
+					StringFunc: func(i int) (string, error) {
+						return "", errors.New("digitiser error")
+					},
+				},
+			},
+		},
+	}
+
+	for testNum, test := range tests {
+		short, err := test.usecase.Shorten(context.Background(), "https://google.com")
+		if short != "" {
+			t.Fatalf("Expected empty string(short), Got: %s", short)
+		}
+		if !reflect.DeepEqual(err, test.expectedErr) {
+			t.Fatalf("Test: %d\nExpected err: %s\nGot: %s", testNum, test.expectedErr, err)
+		}
+	}
+}
