7071c22b4cb60378ae0811d4ed14988ccb4bf7a7
korneevsergey <seriozhakorneev@gmail.com>
2022-11-18 19:48:31 +0300

    tests
    Refactor grpc server Logs interceptor.

--- pkg/grpc/server/log.go
+++ pkg/grpc/server/log.go
@@ -2,7 +2,7 @@ package server
 
 import (
 	"context"
-	"fmt"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
@@ -15,18 +15,18 @@ var Logs = func(
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler,
 ) (interface{}, error) {
+	start := time.Now()
+
 	l := grpclog.NewLoggerV2(defaultWriter, nil, nil)
 
-	fmt.Println(info.Server)
-
 	resp, err := handler(ctx, req)
+	status := status.Code(err)
 	if err != nil {
-
-		//[GIN] 2022/11/18 - 15:10:18 | 400 |    2.999625ms |      172.27.0.4 | GET      "/testlink"
-
-		l.Infof("method %q failed: %s", info.FullMethod, err)
+		l.Infof("| %d - %s |   %s |   %q\n%s\n",
+			status, status, time.Since(start), info.FullMethod, err)
 	} else {
-		l.Infof("method %q success: %s", info.FullMethod, status.Code(err))
+		l.Infof("| %d - %s |   %s |   %q",
+			status, status, time.Since(start), info.FullMethod)
 	}
 
 	return resp, err
